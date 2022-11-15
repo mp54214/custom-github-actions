5ceb4dd1da8c9301bcfd11de83eadfb736c3dade
Matti Ylén <matti.ylen@gmail.com>
2022-11-15 22:45:34 +0200

    deploy to AWS S3 with docker based custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -16,7 +16,7 @@ function run() {
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     
     core.notice("Hello from my custom javascript action!");
-    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    const websiteURL = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`
     core.setOutput("website-url", websiteURL); // see outputs: in action.yaml
 }
 
