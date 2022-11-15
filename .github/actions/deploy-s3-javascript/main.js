12cac03df96915ad16068e2a6afb974301436407
Matti Ylén <matti.ylen@gmail.com>
2022-11-15 03:01:45 +0200

    upload and deploy now files to AWS S3 buckets

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,18 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-    core.notice('Hello from my custom javascript action!')
+  // 1. Get Some Input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: false });
+  const distFolder = core.getInput("dist-folder", {
+    required: true,
+  });
+
+  // 2. Upload the files
+  const s3Uri = `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  
+  core.notice("Hello from my custom javascript action!");
 }
 
 
