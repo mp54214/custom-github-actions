4c66ab1c200160034892a028f87dbc4183a7abc4
Matti Ylén <matti.ylen@gmail.com>
2022-11-15 22:18:56 +0200

    deploy now files to AWS S3 buckets with javascript custom action and outputs deployment static site url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,18 +4,20 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  // 1. Get Some Input values
-  const bucket = core.getInput("bucket", { required: true });
-  const bucketRegion = core.getInput("bucket-region", { required: false });
-  const distFolder = core.getInput("dist-folder", {
-    required: true,
-  });
+    // 1. Get Some Input values
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: false });
+    const distFolder = core.getInput("dist-folder", {
+        required: true,
+    });
 
-  // 2. Upload the files
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
-  
-  core.notice("Hello from my custom javascript action!");
+    // 2. Upload the files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    
+    core.notice("Hello from my custom javascript action!");
+    const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput("website-url", websiteURL); // see outputs: in action.yaml
 }
 
 
